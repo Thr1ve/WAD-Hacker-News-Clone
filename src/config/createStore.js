8da07718e4041df66fe4fbf1dfa15e9ad2c33eef
Thr1ve <fict1on.buhler@gmail.com>
2016-08-09 20:26:02 -0400

    add redux-thunk

--- src/config/createStore.js
+++ src/config/createStore.js
@@ -1,8 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
 import mainReducer from '../reducers';
 
 export default function configureStore(initialState) {
-  const store = createStore(mainReducer);
+  const store = createStore(
+    mainReducer,
+    applyMiddleware(thunk)
+  );
 
   return store;
 }
