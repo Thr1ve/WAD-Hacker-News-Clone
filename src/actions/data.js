0ab0ce6848008fd3fc2cd4c30c608b1887a0fd17
Thr1ve <fict1on.buhler@gmail.com>
2016-08-12 14:20:59 -0400

    add simple actions and reducer for posts

--- src/actions/data.js
+++ src/actions/data.js
@@ -1,2 +1,14 @@
+import { fetchTopStories } from '../config/firebase';
 
-// export const ACTION = 'ACTION';
+export const IS_FETCHING = 'IS_FETCHING';
+export const isFetching = () => ({ type: IS_FETCHING });
+
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
+export const receivePosts = posts => ({ type: RECEIVE_POSTS, posts });
+
+export const fetchPosts = () => (dispatch, getState) => {
+  dispatch(isFetching());
+  fetchTopStories.then(posts => {
+    dispatch(receivePosts(posts));
+  });
+}
--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,11 +1,23 @@
-// import { } from '../actions';
+import { IS_FETCHING, RECEIVE_POSTS } from '../actions';
 
 const defaultState = {
-  foo: 'bar'
+  posts: [],
+  loading: false
 };
 
 export default function dataReducer(state = defaultState, action) {
   switch(action.type) {
+    case IS_FETCHING:
+      return {
+        ...state,
+        loading: true
+      };
+    case RECEIVE_POSTS:
+      return {
+        ...state,
+        posts: [...action.posts],
+        loading: false
+      };
     default:
       return state;
   }
