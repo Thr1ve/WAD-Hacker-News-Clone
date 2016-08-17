3d95a1c34d9fda2e8b8b333f229634422c8f185f
Thr1ve <fict1on.buhler@gmail.com>
2016-08-17 15:47:11 -0400

    update Post component a bit

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,16 @@
 import React, { PropTypes } from 'react';
 
 const Post = ({ post }) =>
-  <div>
-    <b>Title:</b> {post.title}
+  <div className="media">
+    <div className="media-content">
+      <p>
+        <strong><a href={post.url} target="_blank">{post.title}</a></strong>
+      </p>
+      <small>{post.url}</small>
+    </div>
+    <div className="media-right">
+      View Comments
+    </div>
   </div>;
 
 export default Post;
