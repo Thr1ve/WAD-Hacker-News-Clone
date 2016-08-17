71938e1f958da1823a29569d31e5b4ff6ad1fd86
Thr1ve <fict1on.buhler@gmail.com>
2016-08-16 22:06:03 -0400

    add placeholder header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () =>
+  <div className="nav">
+    <div className="nav-left">
+      <div className="nav-item">Top</div>
+      <div className="nav-item">Best</div>
+      <div className="nav-item">New</div>
+      <div className="nav-item">Ask</div>
+      <div className="nav-item">Show</div>
+      <div className="nav-item">Job</div>
+    </div>
+  </div>;
+
+export default Header;
