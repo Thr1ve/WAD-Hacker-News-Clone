7a8f6002e721205346faab38eca33e64c8a105ee
Thr1ve <fict1on.buhler@gmail.com>
2016-08-17 22:34:23 -0400

    add redux middleware

--- src/config/createStore.js
+++ src/config/createStore.js
@@ -1,12 +1,31 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import addListeners from './addListeners';
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 import mainReducer from '../reducers';
+
+const doNotLog = [
+  'RECEIVE_ITEM'
+];
+
+const logger = createLogger({
+  collapsed: true,
+  predicate: function(getState, action) {
+    if (doNotLog.some(actionName => action.type === actionName)) {
+      return false;
+    }
+    return true;
+  }
+});
 
 export default function configureStore(initialState) {
   const store = createStore(
     mainReducer,
-    applyMiddleware(thunk)
+    compose(
+      applyMiddleware(thunk, logger),
+      // This line simply enables the redux dev-tools chrome extension for our app
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
   );
 
   addListeners(store);
