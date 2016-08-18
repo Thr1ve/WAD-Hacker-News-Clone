4844f799c9e5cd2962764a3d786fbaf1fd0dad67
Thr1ve <fict1on.buhler@gmail.com>
2016-08-18 15:53:21 -0400

    refactor actions

--- src/actions/data.js
+++ src/actions/data.js
@@ -1,14 +1,26 @@
-import { fetchTopStories } from '../config/firebase';
+import { fetchTopStoryIds, fetchItem } from '../config/firebase';
 
 export const IS_FETCHING = 'IS_FETCHING';
 export const isFetching = () => ({ type: IS_FETCHING });
 
-export const RECEIVE_POSTS = 'RECEIVE_POSTS';
-export const receivePosts = posts => ({ type: RECEIVE_POSTS, posts });
+export const RECEIVE_IDS = 'RECEIVE_IDS';
+export const receiveIds = ids => ({ type: RECEIVE_IDS, ids });
 
-export const fetchPosts = () => (dispatch, getState) => {
-  dispatch(isFetching());
-  fetchTopStories.then(posts => {
-    dispatch(receivePosts(posts));
+export const RECEIVE_ITEM = 'RECEIVE_ITEM';
+export const receiveItem = (id, item) => ({ type: RECEIVE_ITEM, id, item });
+
+// This may seem a little redundant, but I'd like to keep the
+// firebase API as separate as possible from this logic
+export const getItem = id => (dispatch, getState) => {
+  fetchItem(id).then(item => {
+    dispatch(receiveItem(id, item));
   });
-}
+};
+
+export const getTopIds = () => (dispatch, getState) => {
+  dispatch(isFetching());
+  fetchTopStoryIds().then(ids => {
+    dispatch(receiveIds(ids));
+    ids.forEach(id => dispatch(getItem(id)));
+  });
+};
