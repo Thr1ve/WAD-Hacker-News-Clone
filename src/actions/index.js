5227ca6617a3a5a0eb1fa952cf1acf05d79be3fb
Thr1ve <fict1on.buhler@gmail.com>
2016-08-19 20:41:01 -0400

    make feedName configurable in init

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,12 +3,12 @@ export * from './ui';
 
 import { getFeedIds, getItems, addVisibleItemIds } from './';
 
-export const init = () => (dispatch, getState) => {
+export const init = (feedName = 'TOP') => (dispatch, getState) => {
   // TODO: check route to verify which list to fetch (i.e. 'website.com/top', 'website.com/show', etc.)
   //    - default should be 'TOP'
   // `dispatch(checkUrlRoute());`
-  dispatch(getFeedIds('TOP'))
+  dispatch(getFeedIds(feedName))
   // TODO: Don't add the entirety of the feed's ids to visibleItemIds -- add actions to handle paging or infinite scroll
     .then(ids => dispatch(getItems(ids)))
-    .then(() => dispatch(addVisibleItemIds(getState().data.ids.TOP)));
+    .then(() => dispatch(addVisibleItemIds(getState().data.ids[feedName])));
 };
