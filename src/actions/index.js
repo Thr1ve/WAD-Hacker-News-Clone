e6f53588cb1407ce9cb6ec4142085b8d3a223b8d
Thr1ve <fict1on.buhler@gmail.com>
2016-08-19 20:09:37 -0400

    make it work for now and add todo

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,8 @@ export const init = () => (dispatch, getState) => {
   // TODO: check route to verify which list to fetch (i.e. 'website.com/top', 'website.com/show', etc.)
   //    - default should be 'TOP'
   // `dispatch(checkUrlRoute());`
+  // TODO: Don't add the entirety of the feed to visibleItemIds -- add actions to handle paging or infinite scroll
   dispatch(getTopIds())
     .then(ids => dispatch(getItems(ids)))
-    .then(() => dispatch(addVisibleItemIds(getState().ui.currentFeed)));
+    .then(() => dispatch(addVisibleItemIds(getState().data.ids.TOP)));
 };
