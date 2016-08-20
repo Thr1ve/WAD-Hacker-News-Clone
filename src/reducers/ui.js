abefe68d2c1c7b4515a76f7bf01845fbfbbb66bc
Thr1ve <fict1on.buhler@gmail.com>
2016-08-20 14:31:21 -0400

    cleanup

--- src/reducers/ui.js
+++ src/reducers/ui.js
@@ -11,25 +11,13 @@ const defaultState = {
 export default function uiReducer(state = defaultState, action) {
   switch(action.type) {
     case ADD_VISIBLE_ITEM_IDS:
-      return {
-        ...state,
-        visibleItemIds: state.visibleItemIds.concat(action.ids)
-      };
+      return { ...state, visibleItemIds: state.visibleItemIds.concat(action.ids) };
     case SET_VISIBLE_ITEM_IDS:
-      return {
-        ...state,
-        visibleItemIds: [...action.ids]
-      };
+      return { ...state, visibleItemIds: [...action.ids] };
     case DUMP_VISIBLE_ITEM_IDS:
-      return {
-        ...state,
-        visibleItemIds: []
-      }
+      return { ...state, visibleItemIds: [] }
     case SET_FEED:
-      return {
-        ...state,
-        currentFeed: action.feed
-      };
+      return { ...state, currentFeed: action.feed };
     default:
       return state;
   }
