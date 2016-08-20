b3b1e285ef37805c1db21addb074524971631fe3
Thr1ve <fict1on.buhler@gmail.com>
2016-08-20 19:24:58 -0400

    add PostComponent to App / refactor

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
 
-import { initFeed, nextPage, previousPage } from './actions';
+import { initFeed, nextPage, previousPage, setPage, } from './actions';
+import { getLastPage } from './reducers';
+
 import PostsList from './components/PostsList';
+import PageSelector from './components/PageSelector';
 import Header from './components/Header';
 
 class App extends Component {
@@ -11,9 +14,9 @@ class App extends Component {
     this.props.dispatch(initFeed());
   }
 
-  createHandler = () => feedName => () => {
-    this.props.dispatch(initFeed(feedName));
-  }
+  createInitFeedFactory = () => feedName => () => this.props.dispatch(initFeed(feedName))
+
+  createPageGotoFactory = () => pageNumber => () => this.props.dispatch(setPage(pageNumber))
 
   next = () => this.props.dispatch(nextPage())
 
@@ -23,18 +26,26 @@ class App extends Component {
     return (
       <div className="container">
         <Header
-          createClickHandler={this.createHandler()}
-          next={this.next}
-          prev={this.prev}
+          createClickHandler={this.createInitFeedFactory()}
         />
         <PostsList />
+        <PageSelector
+          createClickHandler={this.createPageGotoFactory()}
+          next={this.next}
+          prev={this.prev}
+          nPages={this.props.nPages}
+          currentPage={this.props.currentPage}
+        />
       </div>
     );
   }
 }
 
 function mapStateToProps(state) {
-  return state;
+  return {
+    nPages: getLastPage(state),
+    currentPage: state.ui.currentPage
+  };
 }
 
 export default connect(mapStateToProps)(App);
