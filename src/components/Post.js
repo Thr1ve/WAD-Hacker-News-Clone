781305e63dddc42abeece9d857a1ab1758ddaf97
Thr1ve <fict1on.buhler@gmail.com>
2016-08-20 05:35:05 -0400

    format html correctly in subtext when necessary

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,13 @@
 import React, { PropTypes } from 'react';
 
-// We could easily make these separate components if needed, but for now that's overkill
+const createMarkup = html => ({ __html: html });
+
+// We could easily make these two functions separate components if needed, but for now that's overkill
+// NOTE: We are relying on HN API to provide safe markup for now. It's trustworthy in
+// my opinion, but I'd still rather not be doing this. Is there a simple way to avoid?
 const getTitle = post => !!post.url ? <a href={post.url} target="_blank">{post.title}</a> : post.title;
-const getSubtext = post => !!post.url ? post.url : post.text;
+const getSubtext = post => !!post.url ? post.url :
+  <div className="content" dangerouslySetInnerHTML={createMarkup(post.text)} />;
 
 const Post = ({ post }) =>
   <div className="media">
