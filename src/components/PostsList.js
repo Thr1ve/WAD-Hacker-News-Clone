4008d806259d9e7e252204333d745affbf4b0fc5
Thr1ve <fict1on.buhler@gmail.com>
2016-08-19 21:18:20 -0400

    add loading div

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -7,22 +7,27 @@ import Post from './Post';
 const PostsList = React.createClass({
   render() {
     return (
-      <div className="container">
-        {
-          this.props.visibleIds.map((id, i) => {
-            if (this.props.data[id]) {
-              return <Post key={i} post={this.props.data[id]} />
-            }
-            return <Post key={i} post={{url: "loading", title: "loading"}} />;
-          })
-        }
-      </div>
-    )
+      this.props.isLoading ?
+        <div className="container">
+          LOADING...
+        </div> :
+        <div className="container">
+          {
+            this.props.visibleIds.map((id, i) => {
+              if (this.props.data[id]) {
+                return <Post key={i} post={this.props.data[id]} />
+              }
+              return <Post key={i} post={{url: "loading", title: "loading"}} />;
+            })
+          }
+        </div>
+    );
   }
 });
 
 function mapStateToProps(state) {
   return {
+    isLoading: state.data.loading,
     data: state.data.cachedItems,
     visibleIds: state.ui.visibleItemIds
   };
