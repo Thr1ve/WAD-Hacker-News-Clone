7fba07d278e2125afffd76723c1a033df13f94a3
Thr1ve <fict1on.buhler@gmail.com>
2016-08-20 23:25:28 -0400

    implement rangeAround

--- __tests__/lib/utils.js
+++ __tests__/lib/utils.js
@@ -1,7 +1,27 @@
-import { range } from '../../src/lib/utils';
+import { simpleRange, rangeAround } from '../../src/lib/utils';
 
 describe('range', () => {
   it('gives a range from 1 to n (inclusive) when called with one argument', () => {
-    expect(range(4)).toEqual([1, 2, 3, 4])
+    expect(simpleRange(4)).toEqual([1, 2, 3, 4]);
+  });
+  it('gives a range from a to b (inclusive) when called with two arguments', () => {
+    expect(simpleRange(4, 8)).toEqual([4, 5, 6, 7]);
+  });
+  it('should throw when given incorrect inputs', () => {
+    expect(simpleRange(4, 8)).toEqual([4, 5, 6, 7]);
+  });
+});
+
+describe('rangeAround', () => {
+  it('gives a range around a given number in ascending order (odd length)', () => {
+    expect(rangeAround(8, 5)).toEqual([6, 7, 8, 9, 10]);
+  });
+  it('is biased to the left when given an even range', () => {
+    expect(rangeAround(8, 4)).toEqual([6, 7, 8, 9]);
+  });
+  it('should ignore numbers less than or equal to 0', () => {
+    expect(rangeAround(2, 4)).toEqual([1, 2, 3]);
+    expect(rangeAround(1, 4)).toEqual([1, 2]);
+    expect(rangeAround(1, 3)).toEqual([1, 2]);
   });
 });
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,8 +5,18 @@ export function objectifyItemArray(arr) {
   }, {});
 }
 
-export function range(start, end) {
+export function simpleRange(start, end) {
   if (arguments.length === 1) {
     return Array.from({ length: start }, (v, i) => i + 1);
+  } else if (end < start) {
+    throw new Error('end must be greater than start');
   }
+  return Array.from({ length: end - start }, (v, i) => i + start);
 }
+
+const oneOrAbove = (n) => n > 0 ? n : 1;
+
+export const rangeAround = (middle, length) => simpleRange(
+  oneOrAbove(middle - Math.floor(length / 2)),
+  middle + Math.ceil(length / 2)
+);
