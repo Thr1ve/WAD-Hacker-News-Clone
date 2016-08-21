a949dc116005eca885cf5e2145e43227913f77a7
Thr1ve <fict1on.buhler@gmail.com>
2016-08-21 01:16:11 -0400

    add simplistic shallow-render test for Post

--- __tests__/components/Post.test.js
+++ __tests__/components/Post.test.js
@@ -1,6 +1,21 @@
+jest.unmock('../../src/components/Post');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import Post from '../../src/components/Post';
 
 describe('Post', () => {
-  it('exists', () => {
-    expect(true).toEqual(true);
+  it('is contained within a div', () => {
+    let renderer = TestUtils.createRenderer();
+    let post;
+
+    renderer.render(
+      <Post post={{ url: "www.google.com", title: "google" }}/>
+    );
+
+    post = renderer.getRenderOutput();
+
+    expect(post.type).toEqual('div');
   });
 });
