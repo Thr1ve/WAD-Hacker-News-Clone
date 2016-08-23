a857fb406ce104da26aa993e69319824be543425
Thr1ve <fict1on.buhler@gmail.com>
2016-08-23 18:41:25 -0400

    rename function in Header test

--- __tests__/components/Header.test.js
+++ __tests__/components/Header.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Header from '../../src/components/Header';
 
-const onClick = () => {};
-const createOnClick = () => onClick;
+const noOp = () => {};
+const createNoOp = () => noOp;
 
 describe('Header:snapshot', () => {
   it('should render correctly', () => {
     const component = renderer.create(
-      <Header createClickHandler={createOnClick}/>
+      <Header createClickHandler={createNoOp}/>
     )
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
