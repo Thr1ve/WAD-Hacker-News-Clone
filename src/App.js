e1ce395065d9420da1579598fc8908b346b5ac9d
Thr1ve <fict1on.buhler@gmail.com>
2016-08-23 19:45:45 -0400

    remove postsList class

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
             createClickHandler={this.createInitFeedFactory()}
           />
         </div>
-        <div className="hero-body postsList">
+        <div className="hero-body">
           <PostsList />
         </div>
         <div className="hero-foot">
