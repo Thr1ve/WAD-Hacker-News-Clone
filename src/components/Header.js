ff3dd79c1527eb49d578bd4d17dfa8627e6e7ebd
Thr1ve <fict1on.buhler@gmail.com>
2016-08-23 19:14:29 -0400

    update header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,24 @@ const Header = ({ createClickHandler, next, prev }) =>
   <nav className="nav">
     <div className="container">
       <div className="nav-left">
-        <div className="nav-item" onClick={createClickHandler('TOP')}>Top</div>
-        <div className="nav-item" onClick={createClickHandler('BEST')}>Best</div>
-        <div className="nav-item" onClick={createClickHandler('NEW')}>New</div>
-        <div className="nav-item" onClick={createClickHandler('ASK')}>Ask</div>
-        <div className="nav-item" onClick={createClickHandler('SHOW')}>Show</div>
-        <div className="nav-item" onClick={createClickHandler('JOB')}>Job</div>
+        <span className="nav-item" >
+          <a className="button is-link" onClick={createClickHandler('TOP')}>Top</a>
+        </span>
+        <span className="nav-item" >
+          <a className="button is-link" onClick={createClickHandler('BEST')}>Best</a>
+        </span>
+        <span className="nav-item" >
+          <a className="button is-link" onClick={createClickHandler('NEW')}>New</a>
+        </span>
+        <span className="nav-item" >
+          <a className="button is-link" onClick={createClickHandler('ASK')}>Ask</a>
+        </span>
+        <span className="nav-item" >
+          <a className="button is-link" onClick={createClickHandler('SHOW')}>Show</a>
+        </span>
+        <span className="nav-item" >
+          <a className="button is-link" onClick={createClickHandler('JOB')}>Job</a>
+        </span>
       </div>
     </div>
   </nav>
