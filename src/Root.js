47fd7a6955be8426e9015069c2f3f0c0b04801af
Thr1ve <fict1on.buhler@gmail.com>
2016-08-26 19:40:20 -0400

    add Root.js

---
+++ src/Root.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Router } from 'react-router';
+import { Provider } from 'react-redux';
+
+import routes from './config/routes';
+
+const Root = React.createClass({
+  render() {
+    const { store, history } = this.props;
+    return (
+      <Provider store={store}>
+        <Router history={history} routes={routes} />
+      </Provider>
+    );
+  }
+});
+
+export default Root;
