c283bd0500e39ad7547da0c61a7befb7d42aa5f5
Thr1ve <fict1on.buhler@gmail.com>
2016-08-27 04:12:34 -0400

    hackish but working initFeed

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,16 +4,28 @@ export * from './ui';
 import { getFeedIds } from './data';
 import { setFeed, dumpVisibleItemIds, setPage } from './ui';
 
-  // TODO: check route to verify which list to fetch (i.e. 'website.com/top', 'website.com/show', etc.)
-  //    - default should be 'TOP'
-  // `dispatch(checkUrlRoute());`
-  // TODO: add actions for infinite scroll
+// TODO: add actions for infinite scroll
+
+// TODO: Redundant...remove...
+const feeds = {
+  'best': 'BEST',
+  'top': 'TOP',
+  'new': 'NEW',
+  'ask': 'ASK',
+  'show': 'SHOW',
+  'job': 'JOB'
+};
 
 export const initFeed = (feedName = 'TOP') => (dispatch, getState) => {
+  const feed = feeds[feedName];
+  if (feed === undefined) {
+    // redirect to TOP
+    console.log('UNKNOWN FEED');
+  }
   // set the new feed
-  dispatch(setFeed(feedName));
+  dispatch(setFeed(feed));
   // remove the currently displayed items
   dispatch(dumpVisibleItemIds());
   // get the ids for the new feed, then reset to first page
-  dispatch(getFeedIds(feedName)).then(ids => dispatch(setPage(1)));
+  dispatch(getFeedIds(feed)).then(ids => dispatch(setPage(1)));
 };
