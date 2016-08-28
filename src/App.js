142cb90a63a93686d2ba84ec0bd064077af09245
Thr1ve <fict1on.buhler@gmail.com>
2016-08-28 00:18:10 -0400

    move loading logic out of App.js

--- src/App.js
+++ src/App.js
@@ -1,60 +1,18 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
-import { initFeed, nextPage, previousPage, setPage, } from './actions';
-import { getLastPage } from './reducers';
-
-import PostsList from './components/PostsList';
-import PageSelector from './components/PageSelector';
 import Header from './components/Header';
 
-class App extends Component {
-  componentWillReceiveProps(nextProps, ...args) {
-    if (this.props.params.feedName !== nextProps.params.feedName) {
-      this.props.dispatch(initFeed(this.props.params.feedName));
-    }
-  }
+const App  = ({ body, footer }) =>
+  <div className="hero is-fullheight">
+    <div className="hero-head">
+      <Header />
+    </div>
+    <div className="hero-body">
+      {body}
+    </div>
+    <div className="hero-foot">
+      {footer}
+    </div>
+  </div>;
 
-  componentDidMount() {
-    this.props.dispatch(initFeed(this.props.params.feedName));
-  }
-
-  initFeedFactory = feedName => () => this.props.dispatch(initFeed(feedName))
-
-  pageGotoFactory = pageNumber => () => this.props.dispatch(setPage(pageNumber))
-
-  next = () => this.props.dispatch(nextPage())
-
-  prev = () => this.props.dispatch(previousPage())
-
-  render() {
-    return (
-      <div className="hero is-fullheight">
-        <div className="hero-head">
-          <Header />
-        </div>
-        <div className="hero-body">
-          <PostsList />
-        </div>
-        <div className="hero-foot">
-          <PageSelector
-            createClickHandler={this.pageGotoFactory}
-            next={this.next}
-            prev={this.prev}
-            nPages={this.props.nPages}
-            currentPage={this.props.currentPage}
-          />
-        </div>
-      </div>
-    );
-  }
-}
-
-function mapStateToProps(state) {
-  return {
-    nPages: getLastPage(state),
-    currentPage: state.ui.currentPage
-  };
-}
-
-export default connect(mapStateToProps)(App);
+export default App;
