0161d9649157f975a8b9d2bbffa7b50f1d55c9b7
Thr1ve <fict1on.buhler@gmail.com>
2016-08-28 08:23:23 -0400

    use Link in PageSelector

--- src/components/PageSelector.js
+++ src/components/PageSelector.js
@@ -1,24 +1,29 @@
 import React, { PropTypes } from 'react';
 import { rangeAround } from '../lib';
+import { Link } from 'react-router';
 
 // NOTE: I want this to end up being an optional prop to the component instead
 // of a constant; I'd like to find a way to have the component try
 // to calculate this on its own by default.
 const MAX_PAGE_SPACE = 7;
 
-const PageSelector = ({ createClickHandler, prev, next, nPages = 1, currentPage = 1 }) =>
+const safePrev = currentPage => currentPage <= 2 ? 1 : currentPage - 1;
+const safeNext = (currentPage, nPages) => currentPage < nPages ? currentPage + 1 : nPages;
+
+const PageSelector = ({ route, nPages = 1, currentPage = 1 }) =>
   <nav className="nav">
     <div className="pagination container">
-      <a onClick={prev} className="button">Previous</a>
-      <a onClick={next} className="button">Next</a>
+      <Link to={route} query={{page: safePrev(currentPage)}} className="button"> Previous </Link>
+      <Link to={route} query={{page: safeNext(currentPage, nPages)}} className="button"> Next </Link>
       <ul>
         {
-          rangeAround(currentPage, MAX_PAGE_SPACE).map(pageNumber =>
-            <li key={pageNumber}>
-              <a
+          rangeAround(currentPage, MAX_PAGE_SPACE).map((pageNumber, i) =>
+            <li key={i}>
+              <Link
+                to={route}
+                query={{ page: pageNumber }}
                 className={`button is-hidden-mobile${pageNumber === currentPage ? ' is-primary' : ''}`}
-                onClick={createClickHandler(pageNumber)}
-              > {pageNumber} </a>
+              > {pageNumber} </Link>
             </li>
           )
         }
@@ -27,9 +32,7 @@ const PageSelector = ({ createClickHandler, prev, next, nPages = 1, currentPage 
   </nav>;
 
 PageSelector.propTypes = {
-  createClickHandler: PropTypes.func,
-  prev: PropTypes.func,
-  next: PropTypes.func,
+  route: PropTypes.string,
   nPages: PropTypes.number,
   currentPage: PropTypes.number
 }
