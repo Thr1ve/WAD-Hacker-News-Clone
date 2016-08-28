db0145356207be14d59f46045c75a7077a1fd71f
Thr1ve <fict1on.buhler@gmail.com>
2016-08-28 06:58:05 -0400

    make getLastPage return whole number

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,7 +8,7 @@ const PAGE_SIZE = 8;
 export default combineReducers({ data, ui });
 
 export const getLastPage = state =>
-  getFeedIds(state.data, state.ui.currentFeed).length / Math.ceil(PAGE_SIZE);
+  Math.ceil(getFeedIds(state.data, state.ui.currentFeed).length / PAGE_SIZE);
 
 export const getPageIds = (state, n) => getFeedIds(state.data, state.ui.currentFeed)
   .slice((n - 1) * PAGE_SIZE, (n - 1) * PAGE_SIZE + PAGE_SIZE);
