3ca18ab120502afbc02608e91fd76651dd80c691
Thr1ve <fict1on.buhler@gmail.com>
2016-09-08 13:28:05 -0400

    add Footer and comments route

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,11 +4,14 @@ import { Route, IndexRedirect } from 'react-router';
 import App from '../App';
 import PostsList from '../containers/PostsList';
 import PageSelector from '../containers/PageSelector';
+import ThreadView from '../containers/ThreadView';
+
+const Footer = () => <div> footer </div>
 
 // TODO: handle pageNumber here as well
 const Routes = (
   <Route path={'/'} component={App}>
-    {/* <Route path={'/comments/:itemId'} component={CommentView} /> */}
+    <Route path={'/comments/:itemId'} component={{ body: ThreadView, footer: Footer }} />
     <Route path={'/feed(/:feedName)'} components={{ body: PostsList, footer: PageSelector }} />
     <IndexRedirect to="feed/top" />
   </Route>
